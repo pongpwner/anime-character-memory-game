0f88e430063a484139750b6743cbeb13bc068cbb
unknown <ericsshyus@gmail.com>
2022-10-25 16:35:39 -0400

    add instructions

--- src/components/game-over/game-over.jsx
+++ src/components/game-over/game-over.jsx
@@ -5,6 +5,21 @@ const GameOver = ({ best, score, handleClick, count, waifu }) => {
   return (
     <div className="game-over">
       <div className="modal-container">
+        <div className="instruction-container">
+          <h2>Instructions:</h2>
+          <div className="instructions">
+            You lose if you let the timer run out, or if you click the same
+            waifu twice.
+          </div>
+          <div className="instructions">
+            The time left will be added to your score after every sucessful
+            pick.
+          </div>
+          <div className="instructions">
+            Picking everything correctly yeilds a large amount of bonus points.
+          </div>
+        </div>
+
         <div className="best-score">Best:{best}</div>
         <div className="score">You Scored:{score}</div>
         {count === WAIFUS.length ? <div>You remembered everyone!</div> : null}
