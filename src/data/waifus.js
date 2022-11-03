33642cf6be34578ba2492f6ad7f83f1fef5e011a
unknown <ericsshyus@gmail.com>
2022-11-02 20:15:23 -0400

    add male array

--- src/data/waifus.js
+++ src/data/waifus.js
@@ -1,35 +1,69 @@
-import image0 from "../assets/images/mai-sakurajima.jpg";
-import image1 from "../assets/images/marin-kitagawa.jpg";
-import image2 from "../assets/images/chizuru-mizuhara.jpg";
-import image3 from "../assets/images/erza-scarlet.jpg";
-import image4 from "../assets/images/yor-forger.jpg";
-import image5 from "../assets/images/utaha-kasumigaoka.jpg";
-import image6 from "../assets/images/ai-hayasaka.jpg";
-import image7 from "../assets/images/akame.jpg";
-import image8 from "../assets/images/asuka-soryu-langley.jpg";
-import image9 from "../assets/images/asuna-yuuki.jpg";
-import image10 from "../assets/images/cc.jpg";
-import image11 from "../assets/images/chihaya-ayase.jpg";
-import image12 from "../assets/images/chitoge-kirisaki.jpg";
-import image13 from "../assets/images/echidna.jpg";
-import image14 from "../assets/images/eiko-tsukimi.jpg";
-import image15 from "../assets/images/erina-nakiri.jpg";
-import image16 from "../assets/images/ganyu.jpg";
-import image17 from "../assets/images/hitagi-senjougahara.jpg";
-import image18 from "../assets/images/hori-kyouko.jpg";
-import image19 from "../assets/images/jabami-yumeko.jpg";
-import image20 from "../assets/images/makise-kurisu.jpg";
-import image21 from "../assets/images/mashiro-shiina.jpg";
-import image22 from "../assets/images/miyuki-shiba.jpg";
-import image23 from "../assets/images/momoi-satsuki.jpg";
-import image24 from "../assets/images/nami.jpg";
-import image25 from "../assets/images/rin-tohsaka.jpg";
-import image26 from "../assets/images/sena-kashiwazaki.jpg";
-import image27 from "../assets/images/shouko-komi.jpg";
-import image28 from "../assets/images/suzune-horikita.jpg";
-import image29 from "../assets/images/yukino-yukinoshita.jpg";
-import image30 from "../assets/images/yuno-gasai.jpg";
-import image31 from "../assets/images/yunyun.jpg";
+//females
+import image0 from "../assets/images/female/mai-sakurajima.jpg";
+import image1 from "../assets/images/female/marin-kitagawa.jpg";
+import image2 from "../assets/images/female/chizuru-mizuhara.jpg";
+import image3 from "../assets/images/female/erza-scarlet.jpg";
+import image4 from "../assets/images/female/yor-forger.jpg";
+import image5 from "../assets/images/female/utaha-kasumigaoka.jpg";
+import image6 from "../assets/images/female/ai-hayasaka.jpg";
+import image7 from "../assets/images/female/akame.jpg";
+import image8 from "../assets/images/female/asuka-soryu-langley.jpg";
+import image9 from "../assets/images/female/asuna-yuuki.jpg";
+import image10 from "../assets/images/female/cc.jpg";
+import image11 from "../assets/images/female/chihaya-ayase.jpg";
+import image12 from "../assets/images/female/chitoge-kirisaki.jpg";
+import image13 from "../assets/images/female/echidna.jpg";
+import image14 from "../assets/images/female/eiko-tsukimi.jpg";
+import image15 from "../assets/images/female/erina-nakiri.jpg";
+import image16 from "../assets/images/female/ganyu.jpg";
+import image17 from "../assets/images/female/hitagi-senjougahara.jpg";
+import image18 from "../assets/images/female/hori-kyouko.jpg";
+import image19 from "../assets/images/female/jabami-yumeko.jpg";
+import image20 from "../assets/images/female/makise-kurisu.jpg";
+import image21 from "../assets/images/female/mashiro-shiina.jpg";
+import image22 from "../assets/images/female/miyuki-shiba.jpg";
+import image23 from "../assets/images/female/momoi-satsuki.jpg";
+import image24 from "../assets/images/female/nami.jpg";
+import image25 from "../assets/images/female/rin-tohsaka.jpg";
+import image26 from "../assets/images/female/sena-kashiwazaki.jpg";
+import image27 from "../assets/images/female/shouko-komi.jpg";
+import image28 from "../assets/images/female/suzune-horikita.jpg";
+import image29 from "../assets/images/female/yukino-yukinoshita.jpg";
+import image30 from "../assets/images/female/yuno-gasai.jpg";
+import image31 from "../assets/images/female/yunyun.jpg";
+//males
+import image100 from "../assets/images/male/alucard.jpg";
+import image101 from "../assets/images/male/archer.jpg";
+import image102 from "../assets/images/male/baki.jpg";
+import image103 from "../assets/images/male/claire-stanfield.jpg";
+import image104 from "../assets/images/male/david-martinez.jpg";
+import image105 from "../assets/images/male/edward-elric.jpg";
+import image106 from "../assets/images/male/eikichi-onizuka.jpg";
+import image107 from "../assets/images/male/gintoki-sakata.jpg";
+import image108 from "../assets/images/male/goku.jpg";
+import image109 from "../assets/images/male/hyakkimaru.jpg";
+import image110 from "../assets/images/male/izuku-midoriya.jpg";
+import image111 from "../assets/images/male/joseph-joestar.jpg";
+import image112 from "../assets/images/male/kaiji-itou.jpg";
+import image113 from "../assets/images/male/kazuma-satou.jpg";
+import image114 from "../assets/images/male/killua-zoldyck.jpg";
+import image115 from "../assets/images/male/koyomi-araragi.jpg";
+import image116 from "../assets/images/male/kuroko-tetsuya.jpg";
+import image117 from "../assets/images/male/lelouch-lamperouge.jpg";
+import image118 from "../assets/images/male/light-yagami.jpg";
+import image119 from "../assets/images/male/mob.jpg";
+import image120 from "../assets/images/male/naruto-uzumaki.jpg";
+import image121 from "../assets/images/male/naruzou-machio.jpg";
+import image122 from "../assets/images/male/ojisan.jpg";
+import image123 from "../assets/images/male/rei-kiriyama.jpg";
+import image124 from "../assets/images/male/rock.jpg";
+import image125 from "../assets/images/male/satoru-gojou.jpg";
+import image126 from "../assets/images/male/shinichi-izumi.jpg";
+import image127 from "../assets/images/male/shirogane-miyuki.jpg";
+import image128 from "../assets/images/male/shouyou-hinata.jpg";
+import image129 from "../assets/images/male/taichi-mashima.jpg";
+import image130 from "../assets/images/male/tanjirou-kamado.jpg";
+import image131 from "../assets/images/male/alucard.jpg";
 
 const WAIFUS = [
   { name: "Mai Sakurajima", imageURL: image0, selected: false, id: 0 },
@@ -64,6 +98,41 @@ const WAIFUS = [
   { name: "Yukino Yukinoshita", imageURL: image29, selected: false, id: 29 },
   { name: "Yuno Gasai", imageURL: image30, selected: false, id: 30 },
   { name: "Yun Yun", imageURL: image31, selected: false, id: 31 },
+];
+
+export const MALES = [
+  { name: "Alucard", imageURL: image100, selected: false, id: 100 },
+  { name: "Archer", imageURL: image101, selected: false, id: 101 },
+  { name: "Baki Hanma", imageURL: image102, selected: false, id: 102 },
+  { name: "Claire Stanfield", imageURL: image103, selected: false, id: 103 },
+  { name: "David Martinez", imageURL: image104, selected: false, id: 104 },
+  { name: "Edward Elric", imageURL: image105, selected: false, id: 105 },
+  { name: "Eikichi Onizuka", imageURL: image106, selected: false, id: 106 },
+  { name: "Gintoki Sakata", imageURL: image107, selected: false, id: 107 },
+  { name: "Goku", imageURL: image108, selected: false, id: 108 },
+  { name: "Hyakkimaru", imageURL: image109, selected: false, id: 109 },
+  { name: "Izuku Midoriya", imageURL: image110, selected: false, id: 110 },
+  { name: "Joseph Joestar", imageURL: image111, selected: false, id: 111 },
+  { name: "Kaiji Itou", imageURL: image112, selected: false, id: 112 },
+  { name: "Kazuma Satou", imageURL: image113, selected: false, id: 113 },
+  { name: "Killua Zoldyck", imageURL: image114, selected: false, id: 114 },
+  { name: "Koyomi Araragi", imageURL: image115, selected: false, id: 115 },
+  { name: "Kuroko Tetsuya", imageURL: image116, selected: false, id: 116 },
+  { name: "Lelouch Lamperouge", imageURL: image117, selected: false, id: 117 },
+  { name: "Light Yagami", imageURL: image118, selected: false, id: 118 },
+  { name: "Mob", imageURL: image119, selected: false, id: 119 },
+  { name: "Naruto Uzumaki", imageURL: image120, selected: false, id: 120 },
+  { name: "Naruzou Machio", imageURL: image121, selected: false, id: 121 },
+  { name: "Ojisan", imageURL: image122, selected: false, id: 122 },
+  { name: "Rei Kiriyama", imageURL: image123, selected: false, id: 123 },
+  { name: "Rock", imageURL: image124, selected: false, id: 124 },
+  { name: "Satoru Gojou", imageURL: image125, selected: false, id: 125 },
+  { name: "Shinichi Izumi", imageURL: image126, selected: false, id: 126 },
+  { name: "Shirogane Miyuki", imageURL: image127, selected: false, id: 127 },
+  { name: "Shouyo Hinata", imageURL: image128, selected: false, id: 128 },
+  { name: "Tachi Mashima", imageURL: image129, selected: false, id: 129 },
+  { name: "Tanjrou Kamado", imageURL: image130, selected: false, id: 130 },
+  { name: "Yoshifumi Nitta", imageURL: image131, selected: false, id: 131 },
 ];
 
 export default WAIFUS;
